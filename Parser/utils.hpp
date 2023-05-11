0af599957b52000b228848e6e5113a24d6d2ce07
joonhyuk.han <virq@naver.com>
2023-05-11 15:48:43 +0900

    feat: Add char argument in ft_split #12

--- Parser/utils.hpp
+++ Parser/utils.hpp
@@ -10,6 +10,6 @@ char* ft_strjoin(char const* s1, char const* s2);
 size_t ft_strlen(const char* str);
 char	*ft_strdup(const char* src);
 std::string ft_strtrim(const std::string& str);
-std::vector<std::string> ft_split(const std::string& str);
+std::vector<std::string> ft_split(const std::string& str, char delimiter);
 
 #endif
