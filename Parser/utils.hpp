7596ec20ce227b89a945897d3a2b552e2d72cf05
joonhyuk.han <virq@naver.com>
2023-05-11 15:38:30 +0900

    feat: Add ft_strtrim #12

--- Parser/utils.hpp
+++ Parser/utils.hpp
@@ -2,9 +2,11 @@
 # define utils_H
 
 #include <cstring>
+#include <string>
 
 char* ft_strjoin(char const* s1, char const* s2);
 size_t ft_strlen(const char* str);
 char	*ft_strdup(const char *src);
+std::string ft_strtrim(const std::string &str);
 
 #endif
