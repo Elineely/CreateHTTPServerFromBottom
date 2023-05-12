bd46c94f670c8f05021c0b0ce27a702963583557
joonhyuk.han <virq@naver.com>
2023-05-12 09:48:58 +0900

    feat: Add headers #12

--- Parser/main.cpp
+++ Parser/main.cpp
@@ -9,6 +9,8 @@ int main(void) {
   Parser parser;
   // struct Request client_request;
   char start_line[] = "GET /index.html HTTP/1.1\r\n";
+  char headers[] = "Server: Cute Web Server \r\nDate: Wed, 11 May 2023 09:10:00 KST \r\nContent-length:10\r\n";
 
   parser.ReadBuffer(start_line);
+  parser.ReadBuffer(headers);
 }
