fb8c0626c0aaf6ce6f784f709afb40c105d8a727
joonhyuk.han <virq@naver.com>
2023-05-12 15:09:13 +0900

    feat: Add detecting two continuous CRLF #12

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -48,25 +48,24 @@ void Parser::ParseFirstLine(void) {
 
   idx1 = input.find_first_of(' ', 0);
   method = input.substr(0, idx1);
-  std::cout << "method:" << method << std::endl;
   if (method != "GET" && method != "POST" && method != "DELETE") {
     this->data_.status = BAD_REQUEST_400;
     throw std::invalid_argument("Method is not acceptable");
   }
+
   idx2 = input.find_first_of(' ', idx1 + 1);
   if (idx2 == std::string::npos) {
     this->data_.status = BAD_REQUEST_400;
     throw std::invalid_argument("There should be a whitespace after method");
   }
   uri = input.substr(idx1 + 1, idx2 - idx1 - 1);
-  std::cout << "uri:" << uri << std::endl;
   if (uri.at(0) != '/') {
     this->data_.status = BAD_REQUEST_400;
     throw std::invalid_argument("URI should start with slash(/)");
   }
+
   http_version =
       input.substr(idx2 + 1, input.find_first_of('\r', idx2 + 1) - idx2 - 1);
-  std::cout << "http_version:" << http_version << std::endl;
   if (http_version != "HTTP/1.0" && http_version != "HTTP/1.1") {
     this->data_.status = BAD_REQUEST_400;
     throw std::invalid_argument("HTTP version should be either 1.0 or 1.1");
@@ -93,6 +92,7 @@ void Parser::SaveBufferInPool(char* buf) {
   } else {
     temp_buf = ft_strdup(this->pool_.total_line);
     delete this->pool_.total_line;
+    // TODO: strjoin 보다 효율적인 문자열 병합 방법이 있는지 찾기
     this->pool_.total_line = ft_strjoin(temp_buf, buf);
     delete temp_buf;
   }
@@ -110,7 +110,19 @@ bool Parser::FindNewlineInPool(void) {
     return (false);
   }
   this->pool_.prev_offset = offset;
-  this->pool_.offset = static_cast<size_t>((find + 2) - total_line);
+  if (std::strncmp(total_line + offset - 2, "\r\n\r\n", 4) == 0) {
+    // TODO: chunked body 인 경우도 확인하기
+    if (this->data_.method == "POST" &&
+        this->data_.headers.find("content-length") !=
+            this->data_.headers.end()) {
+      this->data_.validation_phase = ON_BODY;
+    } else {
+      this->data_.validation_phase = COMPLETE;
+    }
+    this->pool_.offset = static_cast<size_t>((find + 4) - total_line);
+  } else {
+    this->pool_.offset = static_cast<size_t>((find + 2) - total_line);
+  }
   return (true);
 }
 
@@ -131,21 +143,23 @@ void Parser::ParseHeaders(std::map<std::string, std::string>& headers) {
       throw std::invalid_argument("HTTP header value should be one");
     }
 
-    if (headers.find(vec[KEY]) != headers.end()) {
+    key = ft_toLower(vec[KEY]);
+    if (headers.find(key) != headers.end()) {
       this->data_.status = BAD_REQUEST_400;
       throw std::invalid_argument("HTTP header field should not be duplicated");
     }
 
-    key = ft_toLower(vec[KEY]);
     value = ft_strtrim(vec[VALUE]);
     headers[key] = value;
     std::cout << "key:" << key << "/ value:" << value << std::endl;
-  } while (this->FindNewlineInPool() == true);
+  } while (this->FindNewlineInPool() == true &&
+           this->data_.validation_phase == ON_HEADER);
 }
 
 // Public member functions
 void Parser::ReadBuffer(char* buf) {
   try {
+    // CRLF 가 2번으로 마무리되면 더 이상 받지 않도록 처리
     this->SaveBufferInPool(buf);
     if (this->FindNewlineInPool() == false) {
       return;
@@ -160,6 +174,9 @@ void Parser::ReadBuffer(char* buf) {
       case ON_HEADER:
         this->ParseHeaders(this->data_.headers);
         break;
+        // TODO: Body 에서 "HELLO\0WORLD" 와 같은 문자열이 들어왔을 때 어떻게
+        // 처리할 지? (char* vs std::string)
+
       default:
         break;
     }
