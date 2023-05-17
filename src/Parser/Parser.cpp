b524d9c24c82349d1e21e24f2dc1097b975b9aa9
joonhyuk.han <virq@naver.com>
2023-05-17 16:20:57 +0900

    style: Replace local variables  #22
    docs: Add comment in functions

--- src/Parser/Parser.cpp
+++ src/Parser/Parser.cpp
@@ -46,6 +46,7 @@ void Parser::ParseFirstLine(void)
   size_t idx1;
   size_t idx2;
 
+  // 1. HTTP Method 탐색
   idx1 = input.find_first_of(' ', 0);
   method = input.substr(0, idx1);
   if (method != "GET" && method != "POST" && method != "DELETE")
@@ -54,7 +55,9 @@ void Parser::ParseFirstLine(void)
     throw std::invalid_argument("Method is not acceptable");
   }
 
+  // 2. URI 탐색
   idx2 = input.find_first_of(' ', idx1 + 1);
+  // EXCEPTION: Method 뒤에 아무 정보도 없는 경우
   if (idx2 == std::string::npos)
   {
     m_data.status = BAD_REQUEST_400;
@@ -67,6 +70,8 @@ void Parser::ParseFirstLine(void)
     throw std::invalid_argument("URI should start with slash(/)");
   }
 
+  // 3. HTTP Version 탐색
+  // TODO: HTTP 1.0 버전도 호환 가능하게 받을 것인지?
   http_version =
       input.substr(idx2 + 1, input.find_first_of('\r', idx2 + 1) - idx2 - 1);
   if (http_version != "HTTP/1.0" && http_version != "HTTP/1.1")
@@ -74,6 +79,7 @@ void Parser::ParseFirstLine(void)
     m_data.status = BAD_REQUEST_400;
     throw std::invalid_argument("HTTP version should be either 1.0 or 1.1");
   }
+
   m_data.method = method;
   m_data.uri = uri;
   m_data.http_version = http_version;
@@ -90,6 +96,7 @@ void Parser::SaveBufferInPool(char* buf)
   {
     throw std::invalid_argument("NULL string is not allowed");
   }
+
   buf_len = std::strlen(buf);
   m_pool.line_len += buf_len;
   if (m_pool.total_line == NULL)
@@ -110,19 +117,15 @@ void Parser::SaveBufferInPool(char* buf)
 bool Parser::FindNewlineInPool(void)
 {
   const char* find;
-  const char* total_line;
   std::map<std::string, std::string> headers = m_data.headers;
-  size_t offset;
 
-  offset = m_pool.offset;
-  total_line = m_pool.total_line;
-  find = std::strstr(total_line + offset, "\r\n");
+  find = std::strstr(m_pool.total_line + m_pool.offset, "\r\n");
   if (find == NULL)
   {
     return (false);
   }
-  m_pool.prev_offset = offset;
-  if (std::strncmp(total_line + offset - 2, "\r\n\r\n", 4) == 0)
+  m_pool.prev_offset = m_pool.offset;
+  if (std::strncmp(m_pool.total_line + m_pool.prev_offset - 2, "\r\n\r\n", 4) == 0)
   {
     if (m_data.method == "POST" &&
         (headers.find("content-length") != headers.end() ||
@@ -134,11 +137,11 @@ bool Parser::FindNewlineInPool(void)
     {
       m_data.validation_phase = COMPLETE;
     }
-    m_pool.offset = offset + 2;
+    m_pool.offset = m_pool.prev_offset + 2;
   }
   else
   {
-    m_pool.offset = static_cast<size_t>((find + 2) - total_line);
+    m_pool.offset = static_cast<size_t>((find + 2) - m_pool.total_line);
   }
   return (true);
 }
