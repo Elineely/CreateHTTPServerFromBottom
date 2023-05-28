a9f6f4c4854637e758fb74227237c3f4a2225121
Elineely <sooleeny@naver.com>
2023-05-28 11:30:17 +0900

    feat: create a function to check if file or path exist #45

--- include/PathFinder.hpp
+++ include/PathFinder.hpp
@@ -1,12 +1,15 @@
 #ifndef PATHFINDER_HPP
 #define PATHFINDER_HPP
 
+#include <unistd.h>
+
 #include "Response.hpp"
 
 class PathFinder
 {
  private:
   PathFinder();
+  bool checkExist(const std::string& path_or_file);
 
  public:
   PathFinder(Response response_data);
--- src/HttpProcessor/PathFinder.cpp
+++ src/HttpProcessor/PathFinder.cpp
@@ -3,3 +3,8 @@
 PathFinder::PathFinder(/* args */) {}
 
 PathFinder::~PathFinder() {}
+
+bool PathFinder::checkExist(const std::string& path_or_file)
+{
+  return (access(path_or_file.c_str(), F_OK) == 0);
+}
