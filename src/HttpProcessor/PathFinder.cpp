2432e270ddb81da7703dfcbc6a1da4cee5a34518
Elineely <sooleeny@naver.com>
2023-05-29 12:33:01 +0900

    fix: some more of default.config #45

--- src/HttpProcessor/PathFinder.cpp
+++ src/HttpProcessor/PathFinder.cpp
@@ -2,14 +2,14 @@
 // #include "../../include/PathFinder.hpp"
 // #include "PathFinder.hpp"
 
-PathFinder::PathFinder(/* args */) {}
+PathFinder::PathFinder() {}
 
 PathFinder::~PathFinder() {}
 
 PathFinder::PathFinder(Request requset_data, t_server server_data,
                        Response& response_data)
 {
-  string locationBlock = requset_data.uri;
+  std::string locationBlock = requset_data.uri;
 }
 
 bool PathFinder::checkExist(const std::string& path_or_file)
