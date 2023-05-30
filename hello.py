ef0ab8d79c6aea24aa1384a79faae7ab266bf3e2
joonhan <joonhan@student.42seoul.kr>
2023-05-30 20:10:10 +0900

    feat: CGI 실행용 파일 추가 #61

---
+++ hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+print("HTTP/1.1 200 OK\r\n", end='')
+print("Content-type: text/plain\r\n", end='')
+print("Content-length: 11\r\n\r\n", end='')
+
+print("hello world", end='')
+exit(0);
