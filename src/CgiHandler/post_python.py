6bb8f6fe9c5e37cc1097dc7b96581658c19a29f0
ssolpang <iless3377@naver.com>
2023-06-04 23:35:15 +0900

    feat: GET, POST python cgi script draft #80

--- src/CgiHandler/post_python.py
+++ src/CgiHandler/post_python.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/opt/homebrew/bin/python3
+#/usr/bin/python
 
 import cgi
 import os
@@ -6,22 +7,45 @@ import cgitb
 cgitb.enable()
 # # import cgitb; cgitb.enable(display=0, logdir="./log")
 
-# /////////////////////////////////////////////
-# print("Content-type: text/html")
-# print() # end of headers. without \n, Internal Server Error.
-# print('<html>')
-# print('<head>')
-# print('<title>Hello Word - First CGI Program</title>')
-# print('</head>')
-# print('<body>')
+form = cgi.FieldStorage()
 
-# form = cgi.FieldStorage()
+from datetime import datetime
+now = datetime.now()
+default_header = "Content-Type: text/html\r\n\r"
+default_content = "<html><head>\n" + \
+               "<title>cute CGI posting</title>\n" + \
+               "</head><body>\n" + \
+              "<h1>Post Success!</h1>\n" + \
+              "<p>with kkam-bo</p>\n" + \
+              "<p>" + now.strftime('%Y-%m-%d %H:%M:%S') + "</p>\n" + \
+             "</body></html>\n"
 
-# print(form["name"].value)
-# print(form["email"].value)
 
-# print('</body>')
-# print('</html>')
+text_content = input()
+# if (len(text_content) > maxlen): # len(text_content.encode('uft-8))
+	# return error_page
+
+upload_path = os.getenv('X_UPLOAD_PATH')
+
+if os.path.exists(upload_path):
+	upload_path += "/"
+else:
+	upload_path = "./tmp_d/"
+
+if not os.path.exists(upload_path):
+	os.mkdir(upload_path)
+
+new_file = os.getenv('PATH_INFO')
+new_file = upload_path + new_file
+
+with open(new_file) as text_file: # saved path where?
+	text_file.write(text_content)
+
+print(default_header)
+print(default_content)
+# or return text_file?
+
+
 
 # /////////////////////////////////////////////
 
@@ -67,5 +91,28 @@ cgitb.enable()
 # exit()
 
 
-print ("\n***********Test for python CGI************\n")
+# /////////////////////////////////////
+# if os.path.exists("exam.txt"):
+#   os.remove("exam.txt")
+# else:
+#   print("not exist")
 
+# example
+# if "name" not in form or "addr" not in form:
+#     print("<H1>Error</H1>")
+#     print("Please fill in the name and addr fields.")
+
+# print("<p>name:", form["name"].value)
+# print("<p>addr:", form["addr"].value)
+
+# value = form.getlist("username")
+# usernames = ",".join(value)
+
+# fileitem = form["userfile"]
+# if fileitem.file:
+#     # It's an uploaded file; count lines
+#     linecount = 0
+#     while True:
+#         line = fileitem.file.readline()
+#         if not line: break
+#         linecount = linecount + 1
