62b44302d3de3e400e64b05f6398b4cd79810b2d
jkwak <jkwak@student.42seoul.kr>
2023-06-05 14:10:45 +0900

    fix: bianry file delivery ok #80

--- src/CgiHandler/get_python.py
+++ src/CgiHandler/get_python.py
@@ -1,11 +1,12 @@
-#!/opt/homebrew/bin/python3
-#/usr/bin/python
+#!/usr/bin/python
+#/opt/homebrew/bin/python3
 
 # keep in mind that the CGI module in Python has been removed in Python 3,
 # so you may need to use an alternative approach for handling CGI requests.
 
 import cgi
 import os
+import sys
 import cgitb
 cgitb.enable()
 # # import cgitb; cgitb.enable(display=0, logdir="./log")
@@ -23,12 +24,12 @@ default_content = "<html><head>\n" + \
               "<h1>This is my so cute CGI output</h1>\n" + \
               "<p>Bang-gle</p>\n" + \
               "<p>" + now.strftime('%Y-%m-%d %H:%M:%S') + "</p>\n" + \
-             "</body></html>\n"
+             "</body></html>"
 
 #*******************************************#
 # only for '.py' file that we've already set
 #*******************************************#
-print('\n *** CASE OF DEFAULT *** \n')
+# print('\n *** CASE OF DEFAULT *** \n')
 
 print(default_header)
 print(default_content)
@@ -36,16 +37,16 @@ print(default_content)
 #**************************************************************#
 # case of taking text file 'PATH_INFO' env (maybe for cgi tester?)
 #**************************************************************#
-print('\n *** CASE OF TEXT PATH_INFO *** \n')
+# print('\n *** CASE OF TEXT PATH_INFO *** \n')
 
 print("Content-Type: text/html\r\n\r")
-print(os.getenv('PATH_INFO'))
+# print(os.getenv('PATH_INFO'))
 path_info_t = os.getenv('PATH_INFO')
 # with -> no need to use close() function
 if os.path.isfile(path_info_t):
     with open(path_info_t, 'r') as text_file:
         text_content = text_file.read()
-        print(text_content)
+        sys.stdout.write(text_content)
 else:
     print(default_header)
     print(default_content)
@@ -53,27 +54,21 @@ else:
 #**************************************************************#
 # case of taking binary file 'PATH_INFO' env (maybe for cgi tester?)
 #**************************************************************#
-print('\n *** CASE OF BINARY PATH_INFO *** \n')
+# print('\n *** CASE OF BINARY PATH_INFO *** \n')
 
 # pip install pillow
 # from PIL import Image
 # from PIL import ImageDraw
 
 print("Content-Type: image/jpeg\r\n\r")
-path_info_i =  os.getenv('PATH_INFO')
+path_info_i = os.getenv('PATH_INFO')
 if os.path.isfile(path_info_i):
     with open(path_info_i, 'rb') as image_file:
         image_content = image_file.read()
-        print(image_content)     # b'~~~\n'
-        # for b in image_content:  # 97 97 97 10
-        #     print(b)
-        # byte = image_file.read(1) # byte by byte ..
-        # while byte != b"":
-        #     print(byte, end="")
-        #     byte = image_file.read(1)
-# else:
-#     print(default_header)
-#     print(default_content)
+        sys.stdout.write(image_content)
+else:
+    print(default_header)
+    print(default_content)
 
 # img = Image.open(path_info_i)
 # draw = ImageDraw.Draw(img)
--- src/CgiHandler/post_python.py
+++ src/CgiHandler/post_python.py
@@ -1,5 +1,5 @@
-#!/opt/homebrew/bin/python3
-#/usr/bin/python
+#!/usr/bin/python
+#/opt/homebrew/bin/python3
 
 import cgi
 import os
@@ -20,12 +20,17 @@ default_content = "<html><head>\n" + \
               "<p>" + now.strftime('%Y-%m-%d %H:%M:%S') + "</p>\n" + \
              "</body></html>\n"
 
+import sys
+text_content = sys.stdin.readline()
+# print(text_content)
 
-text_content = input()
 # if (len(text_content) > maxlen): # len(text_content.encode('uft-8))
 	# return error_page
 
-upload_path = os.getenv('X_UPLOAD_PATH')
+if os.getenv('X_UPLOAD_PATH'):
+    upload_path = os.getenv('X_UPLOAD_PATH')
+else:
+    upload_path = ""
 
 if os.path.exists(upload_path):
 	upload_path += "/"
@@ -35,62 +40,28 @@ else:
 if not os.path.exists(upload_path):
 	os.mkdir(upload_path)
 
-new_file = os.getenv('PATH_INFO')
+if os.getenv('PATH_INFO'):
+    new_file = os.getenv('PATH_INFO')
+else:
+    new_file = "tmp.t"
 new_file = upload_path + new_file
 
-with open(new_file) as text_file: # saved path where?
+print("upload_path : " + upload_path + ", new_file : " + new_file)
+
+with open(new_file, 'w') as text_file:
 	text_file.write(text_content)
 
+# print(default_header)
+# print(default_content)
+# or
 print(default_header)
-print(default_content)
-# or return text_file?
+with open(new_file, 'r') as text_file:
+    test = text_file.read()
+    sys.stdout.write(test)
 
 
 
 # /////////////////////////////////////////////
-
-# save_path = os.getenv("SAVED_PATH")
-
-# if save_path is None:
-#     save_path = "./tmp/"
-# else:
-#     save_path += "/"
-
-# print("Content-Type: text/html\r")
-
-# if not os.path.exists(save_path):
-#     os.mkdir(save_path)
-
-# # Parse the form data
-# form = cgi.FieldStorage()
-# # Extract the uploaded files
-# files = {}
-# for field in form.keys():
-#     # Check if the field is a file field
-#     if isinstance(form[field], cgi.FieldStorage) and form[field].filename:
-#         # Save the file to the specified location
-#         with open(save_path + form[field].filename, 'wb') as f:
-#             f.write(form[field].file.read())
-#         files[field] = form[field].filename
-
-# if len(files) > 0:
-#     message = "file uploaded success"
-# else:
-#     message = "file uploaded failed"
-
-# # Print the uploaded files
-# response = "<html><body><center>"
-# response += "<h1>{}<h1>".format(message)
-# for field, filename in files.items():
-#     response += "<p>{}: {}</p>\n".format(field, filename)
-# response += "</center></body></html>"
-
-# print("\r")
-# print(response)
-
-# exit()
-
-
 # /////////////////////////////////////
 # if os.path.exists("exam.txt"):
 #   os.remove("exam.txt")
