e06f2a31755dc1b1d77c12448e4f928e709f8124
joonhan <joonhan@student.42seoul.kr>
2023-06-06 11:28:47 +0900

    feat: 로그 출력 시 파일 정보, 줄 번호, 함수명 출력 #101

--- include/Log.hpp
+++ include/Log.hpp
@@ -3,6 +3,8 @@
 
 #include <cstdarg>
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 #include <string>
 
 #define RED_TEXT 	"\033[31m"
@@ -11,14 +13,33 @@
 #define BLUE_TEXT	"\033[34m"
 #define CUT_TEXT	"\033[0m"
 
+#define LOG_ERROR(format, args...) Log::print(ERROR, __FILE__, __func__, __LINE__, format, ## args);
+#define LOG_DEBUG(format, args...) Log::print(DEBUG, __FILE__, __func__, __LINE__, format, ## args);
+#define LOG_INFO(format, args...) Log::print(INFO, __FILE__, __func__, __LINE__, format, ## args);
+
+enum e_log_level
+{
+  ERROR = -1,
+  DEBUG,
+  INFO,
+};
+
 class Log
 {
- public:
-  static void error(const char* message, ...);
-  static void info(const char* message, ...);
-  static void debug(const char* message, ...);
-  static void start(void);
-  static void print(const char* message, va_list ap);
+  private:
+    Log(void);
+    ~Log();
+    Log(const Log& src);
+    Log& operator=(const Log& rhs);
+
+    static void printLogLevel(e_log_level level);
+    static void printCallerInfo(const char* file, const char* function, int line);
+
+  public:
+    static void start(void);
+    static void print(e_log_level level, const char* file, const char* function,
+                      int line, const char* message, ...);
 };
+
 
 #endif
