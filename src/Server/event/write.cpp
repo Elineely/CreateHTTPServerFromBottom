ba89d80628390cfa4b7714647f2d7136a849441c
joonhyuk.han <virq@naver.com>
2023-06-11 04:17:24 +0900

    feat: Add saving log file for client write #139

--- src/Server/event/write.cpp
+++ src/Server/event/write.cpp
@@ -14,7 +14,9 @@ void Server::clientWriteEvent(struct kevent *current_event)
   udata = static_cast<t_event_udata *>(current_event->udata);
   response_write = &udata->m_response_write;
   message = &response_write->message[0];
-  // write(1, message, response_write->length);
+  int log_file = open("./log_file", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  write(log_file, message, response_write->length);
+  close(log_file);
   int send_byte = 0;
   send_byte = send(current_event->ident, message + response_write->offset,
                    response_write->length - response_write->offset, 0);
