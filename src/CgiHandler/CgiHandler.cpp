2a2ea988752cd8c5e3e38f7604ed54fd511ca91a
joonhyuk.han <virq@naver.com>
2023-06-10 14:12:29 +0900

    feat: GET cgi 도 파이프 2개 받고 POST cgi 로직 수정 #139

--- src/CgiHandler/CgiHandler.cpp
+++ src/CgiHandler/CgiHandler.cpp
@@ -40,25 +40,26 @@ void CgiHandler::setCgiEnv(void)
 
   // 값이 없는 경우는 빈 값으로 두면 생략하는 것과 같은 효과
 
-  m_env_list.push_back("GATEWAY_INTERFACE=CGI/1.1");
+  // m_env_list.push_back("GATEWAY_INTERFACE=CGI/1.1");
 
   // m_env_list.push_back("SERVER_NAME=" + SERVER_NAME);
   // m_env_list.push_back("SERVER_PORT=" + SERVER_PORT);
   m_env_list.push_back("SERVER_PROTOCOL=HTTP/1.1");
-  m_env_list.push_back("SERVER_SOFTWARE=cute_webserv/1.0");
+  // m_env_list.push_back("SERVER_SOFTWARE=cute_webserv/1.0");
 
   // m_env_list.push_back("REMOTE_ADDR=127.0.0.1");
   // 클라이언트의 도메인 네임. 없으면 NULL이거나, REMOTE_ADDR의 값으로 대체
   // m_env_list.push_back("REMOTE_HOST=127.0.0.1");
   m_env_list.push_back("REQUEST_METHOD=" + m_request_data.method);
+  m_env_list.push_back("X_SECRET_HEADER_FOR_TEST=1");
 
   if (m_response_data.cgi_bin_path == "")
   {
-    m_env_list.push_back("SCRIPT_NAME=" + defualt_cgi_script);
+    // m_env_list.push_back("SCRIPT_NAME=" + defualt_cgi_script);
   }
   else
   {
-    m_env_list.push_back("SCRIPT_NAME=" + m_response_data.cgi_bin_path);
+    // m_env_list.push_back("SCRIPT_NAME=" + m_response_data.cgi_bin_path);
     // m_env_list.push_back("SCRIPT_NAME=" + m_response_data.file_name);
   }
 
@@ -66,13 +67,13 @@ void CgiHandler::setCgiEnv(void)
   // 이후의 값을 말함 그러나 우린 CGI 스크립트만 실행시킴(실행시킬 스크립트 파일
   // 이외의 파일을 받지 않기 때문에 빈 값) CGI tester에서 혹시 값을 추가로 줄까?
   // 그러면 설정 필요함
-  m_env_list.push_back("PATH_INFO=");
+  m_env_list.push_back("PATH_INFO=/");
   // PATH_INFO가 NULL이면 얘도 NULL (값이 있으면, 서버 내의 실제 주소를 담아주면
   // 됨 ex. usr/www/html ..)
-  m_env_list.push_back("PATH_TRANSLATED=");
+  // m_env_list.push_back("PATH_TRANSLATED=");
 
   // 값을 안 줘도 ""로라도 채워줘야 하는 환경변수
-  m_env_list.push_back("QUERY_STRING=");
+  // m_env_list.push_back("QUERY_STRING=");
 
   if (m_request_data.body.size() != 0)
   {
@@ -85,8 +86,8 @@ void CgiHandler::setCgiEnv(void)
                          m_request_data.headers["content-type"]);
   }
 
-  m_env_list.push_back("X_FILE_PATH=" + m_response_data.file_path);
-  m_env_list.push_back("X_UPLOAD_PATH=" + m_response_data.uploaded_path);
+  // m_env_list.push_back("X_FILE_PATH=" + m_response_data.file_path);
+  // m_env_list.push_back("X_UPLOAD_PATH=" + m_response_data.uploaded_path);
 
   for (int i = 0; i < m_env_list.size(); ++i)
   {
@@ -165,9 +166,17 @@ GetCgiHandler& GetCgiHandler::operator=(GetCgiHandler const& obj)
 
 void GetCgiHandler::pipeAndFork()
 {
+  if (pipe(m_to_child_fds) == RETURN_ERROR)
+  {
+    LOG_ERROR("Failed to create m_to_child_fds pipe");
+    throw PipeForkException();
+  }
+
   if (pipe(m_to_parent_fds) == RETURN_ERROR)
   {
-    LOG_ERROR("Failed to create pipe");
+    LOG_ERROR("Failed to create m_to_parent_fds pipe");
+    close(m_to_child_fds[READ]);
+    close(m_to_child_fds[WRITE]);
     throw PipeForkException();
   }
 
@@ -175,6 +184,8 @@ void GetCgiHandler::pipeAndFork()
   if (m_pid == RETURN_ERROR)
   {
     LOG_ERROR("Failed to fork");
+    close(m_to_child_fds[READ]);
+    close(m_to_child_fds[WRITE]);
     close(m_to_parent_fds[READ]);
     close(m_to_parent_fds[WRITE]);
     throw PipeForkException();
@@ -184,24 +195,35 @@ void GetCgiHandler::pipeAndFork()
 void GetCgiHandler::executeCgi()
 {
   close(m_to_parent_fds[READ]);
+  close(m_to_child_fds[WRITE]);
+
+  if (dup2(m_to_child_fds[READ], STDIN_FILENO) == RETURN_ERROR)
+  {
+    LOG_ERROR("Failed to dup2(%d, %d)", m_to_child_fds[WRITE], STDIN_FILENO);
+    close(m_to_child_fds[WRITE]);
+    throw PipeForkException();
+  }
+  close(m_to_child_fds[READ]);
 
   if (dup2(m_to_parent_fds[WRITE], STDOUT_FILENO) == RETURN_ERROR)
   {
-    LOG_ERROR("Failed to dup2(%d, %d)", m_to_parent_fds, STDOUT_FILENO);
+    LOG_ERROR("Failed to dup2(%d, %d)", m_to_parent_fds[WRITE], STDOUT_FILENO);
     close(m_to_parent_fds[WRITE]);
     throw PipeForkException();
   }
   close(m_to_parent_fds[WRITE]);
 
   setCgiEnv();
-  std::string cgi_file = m_response_data.root_path + "/" + m_response_data.file_name;
-  const char* cgi_bin_path = m_response_data.cgi_bin_path.c_str();
-  const char* argv[] = {cgi_bin_path, cgi_file.c_str(), NULL};
+  // const char* cgi_bin_path = m_response_data.cgi_bin_path.c_str();
+  // std::string cgi_file = m_response_data.root_path + "/" +
+  // m_response_data.file_name;
+  const char* argv[] = {"./cgi_tester", NULL};
   const char** envp = &m_env_list_parameter[0];
-  if (execve(cgi_bin_path, const_cast<char* const*>(argv),
+  if (execve("./cgi_tester", const_cast<char* const*>(argv),
              const_cast<char* const*>(envp)) == RETURN_ERROR)
   {
     // LOG_ERROR("Failed to execve function => strerrno: %s", strerror(errno));
+    std::cerr << "error: " << strerror(errno) << std::endl;
     std::vector<char> error_message = makeErrorPage();
     write(STDOUT_FILENO, &error_message[0], error_message.size());
     exit(EXIT_FAILURE);
@@ -213,13 +235,17 @@ void GetCgiHandler::outsourceCgiRequest(void)
   try
   {
     pipeAndFork();
+
     if (m_pid == CHILD_PROCESS)
     {
       executeCgi();
     }
     else
     {
+      close(m_to_child_fds[READ]);
+      close(m_to_child_fds[WRITE]);
       close(m_to_parent_fds[WRITE]);
+
       m_response_data.read_pipe_fd = m_to_parent_fds[READ];
       m_response_data.cgi_child_pid = m_pid;
     }
@@ -252,15 +278,15 @@ PostCgiHandler& PostCgiHandler::operator=(PostCgiHandler const& obj)
 {
   if (this != &obj)
   {
-               m_request_data = obj.m_request_data;
-               m_response_data = obj.m_response_data;
-               m_env_list = obj.m_env_list;
-               m_env_list_parameter = obj.m_env_list_parameter;
-               m_to_child_fds[READ] = obj.m_to_child_fds[READ];
-               m_to_child_fds[WRITE] = obj.m_to_child_fds[WRITE];
-               m_to_parent_fds[READ] = obj.m_to_parent_fds[READ];
-               m_to_parent_fds[WRITE] = obj.m_to_parent_fds[WRITE];
-               m_pid = obj.m_pid;
+    m_request_data = obj.m_request_data;
+    m_response_data = obj.m_response_data;
+    m_env_list = obj.m_env_list;
+    m_env_list_parameter = obj.m_env_list_parameter;
+    m_to_child_fds[READ] = obj.m_to_child_fds[READ];
+    m_to_child_fds[WRITE] = obj.m_to_child_fds[WRITE];
+    m_to_parent_fds[READ] = obj.m_to_parent_fds[READ];
+    m_to_parent_fds[WRITE] = obj.m_to_parent_fds[WRITE];
+    m_pid = obj.m_pid;
   }
   return (*this);
 }
@@ -320,11 +346,12 @@ void PostCgiHandler::executeCgi()
   close(m_to_parent_fds[WRITE]);
 
   setCgiEnv();
-  const char* cgi_bin_path = m_response_data.cgi_bin_path.c_str();
-  std::string cgi_file = m_response_data.root_path + "/" + m_response_data.file_name;
-  const char* argv[] = {cgi_bin_path, cgi_file.c_str(), NULL};
+  // const char* cgi_bin_path = m_response_data.cgi_bin_path.c_str();
+  // std::string cgi_file = m_response_data.root_path + "/" +
+  // m_response_data.file_name;
+  const char* argv[] = {"./cgi_tester", NULL};
   const char** envp = &m_env_list_parameter[0];
-  if (execve(cgi_bin_path, const_cast<char* const*>(argv),
+  if (execve("./cgi_tester", const_cast<char* const*>(argv),
              const_cast<char* const*>(envp)) == RETURN_ERROR)
   {
     // LOG_ERROR("Failed to execve function => strerrno: %s", strerror(errno));
@@ -347,8 +374,10 @@ void PostCgiHandler::outsourceCgiRequest(void)
     else
     {
       close(m_to_child_fds[READ]);
-      close(m_to_child_fds[WRITE]);
       close(m_to_parent_fds[WRITE]);
+
+      write(m_to_child_fds[WRITE], &m_request_data.body[idx], m_request_data.body.size());
+      close(m_to_child_fds[WRITE]);
 
       m_response_data.read_pipe_fd = m_to_parent_fds[READ];
       m_response_data.cgi_child_pid = m_pid;
