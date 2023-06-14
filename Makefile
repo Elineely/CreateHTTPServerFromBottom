0927059671c5041118006cfbc374b0ace577b329
joonhan <joonhan@student.42seoul.kr>
2023-06-14 13:49:43 +0900

    chore: Remove unused files #149
    Co-authored-by: kipark <Arkingco@gmail.com>

--- Makefile
+++ Makefile
@@ -48,9 +48,6 @@ SRC_SERVER_FILES		:=	Server.cpp 				\
 SRC_UTILS_DIR			:=	utils/
 SRC_UTILS_FILES			:=	ft_config_split.cpp		\
 							ft_split.cpp			\
-							ft_strdup.cpp			\
-							ft_strjoin.cpp			\
-							ft_strlen.cpp			\
 							ft_strtrim.cpp			\
 							ft_toLower.cpp			\
 							ft_error.cpp			\
--- include/utils.hpp
+++ include/utils.hpp
@@ -18,9 +18,6 @@ struct tolower_functor
   char operator()(char c) const { return std::tolower(c); }
 };
 
-char *ft_strjoin(char const *s1, char const *s2);
-size_t ft_strlen(const char *str);
-char *ft_strdup(const char *src);
 std::string ft_strtrim(const std::string &str);
 std::vector<std::string> ft_split(const std::string &str, char delimiter,
                                   int count);
