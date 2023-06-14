109d7df5d52f8744a8fd0e196650299b9d654f88
joonhan <joonhan@student.42seoul.kr>
2023-06-14 15:43:30 +0900

    test: Add tmp_file name #149

--- src/HttpProcessor/MethodHandler.cpp
+++ src/HttpProcessor/MethodHandler.cpp
@@ -220,7 +220,7 @@ void PostMethodHandler::methodRun()
 
   // TODO: /post_body 일 때 파일 저장 안되는 현상 수정하기
   std::string target_file(m_response_data.file_path +
-                          m_response_data.file_name);
+                          m_response_data.file_name + "tmp_file");
   // delete the target file
   if (m_response_data.file_exist == true)
   {
