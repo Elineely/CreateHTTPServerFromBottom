654aaba80cb9f95e23662dbae0ac82499d813234
joonhan <joonhan@student.42seoul.kr>
2023-06-15 12:02:04 +0900

    fix: Replace Content-type to Content-Type

--- data/cgi/hello.py
+++ data/cgi/hello.py
@@ -4,14 +4,13 @@ import sys
 import time
 
 print("HTTP/1.1 200 OK\r\n", end='')
-print("Content-type: text/plain\r\n\r\n", end='')
+print("Status: 200\r\n", end='')
+print("Content-Type: text/plain\r\n\r\n", end='')
 
 request_method = os.environ.get('REQUEST_METHOD', '')
 upload_path = os.environ.get('X_UPLOAD_PATH', '')
 if request_method == "GET":
     print("GOTTA HOME!", end='')
 elif request_method == "POST":
-    with open(upload_path + "/" + "new_file", 'w') as text_file:
-	    text_file.write("this is open!")
     print("upload path", upload_path)
     print("GOTTA POST!", end='')
