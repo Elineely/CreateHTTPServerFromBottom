6f29a0b6941858c06fd195c6156340779a64b922
joonhan <joonhan@student.42seoul.kr>
2023-06-15 17:16:05 +0900

    feat: Add cgi process code #170

--- data/cgi/post_test.py
+++ data/cgi/post_test.py
@@ -5,42 +5,28 @@ import sys
 
 import cgitb
 cgitb.enable()
-# # import cgitb; cgitb.enable(display=0, logdir="./log")
 
-# Set the directory to store the uploaded image
 upload_dir = './data/upload/'
 
-# Create an instance of FieldStorage
-# form = cgi.FieldStorage()
+form = cgi.FieldStorage()
 
-# Get the image file from the form data
-# if 'image' in form:
-    # image = form['image']
+if 'image' in form:
+    image = form['image']
 
-    # if image.value:
-        # Create a unique value for the image
-        # target_path = os.path.join(upload_dir, image.value)
-target_path = upload_dir + "temp.jpg"
-print(target_path, file=sys.stderr)
-# Save the image to the upload directory
-image_data = sys.stdin.buffer.read()
-# print(image_data, file=sys.stderr)
-with open(target_path, 'wb') as a_file:
-    a_file.write(image_data)
-print('Content-Type:text/html\r\n', end='')
-print('Status: 200\r\n\r\n', end='')
-print('<html><body>', end='')
-print('<h2>Image saved successfully!</h2>', end='')
-print('</body></html>', end='')
-#     else:
-#         print('Content-Type:text/html\r\n', end='')
-#         print('Status: 200\r\n\r\n', end='')
-#         print('<html><body>', end='')
-#         print('<h2>No image file selected!</h2>', end='')
-#         print('</body></html>', end='')
-# else:
-#     print('Content-Type:text/html\r\n', end='')
-#     print('Status: 200\r\n\r\n', end='')
-#     print('<html><body>', end='')
-#     print('<h2>No image file found in the form data!</h2>', end='')
-#     print('</body></html>', end='')
+    if image.filename:
+        # Create a unique filename for the image
+        save_path = os.path.join(upload_dir, image.filename)
+        with open(save_path, 'wb') as a_file:
+            a_file.write(image.file.read())
+
+        print('Content-Type:text/html\r')
+        print('Status: 200\r\n\r')
+        print('<html><body>')
+        print('<h2>Image saved successfully!</h2>')
+        print('</body></html>')
+else:
+    print('Content-Type:text/html\r')
+    print('Status: 500\r\n\r')
+    print('<html><body>')
+    print('<h2>No image file found in the form data!</h2>')
+    print('</body></html>')
